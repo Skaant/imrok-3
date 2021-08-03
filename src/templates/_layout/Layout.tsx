92e4616bd26d3b06c0cf527e3093f80b31e9ce69
Rimarok <romaric.ruga@gmail.com>
2021-08-03 11:14:38 +0200

    Layout: add title from siteMetadata

--- src/templates/_layout/Layout.tsx
+++ src/templates/_layout/Layout.tsx
@@ -1,13 +1,33 @@
+import { graphql, useStaticQuery } from "gatsby";
 import React, { ReactElement } from "react";
 import { Helmet } from "react-helmet";
 import Navbar from "./Navbar";
 
-function Layout({ children }: { children: ReactElement }) {
+function Layout({
+  title,
+  description,
+  tags,
+  children
+}: {
+  title?: string
+  description?: string
+  tags?: string[]
+  children: ReactElement
+}) {
+  const data = useStaticQuery(graphql`
+    query LayoutQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
   return (
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>IMROK</title>
+        <title>{ title ? title + ' - ' : '' }{ data.site.siteMetadata.title }</title>
       </Helmet>
       <Navbar />
       <div id="content" className="container-fluid">
