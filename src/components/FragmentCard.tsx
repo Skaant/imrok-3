e39d2918fba9c255e4bd5f6a1f8b3d338b650119
Rimarok <romaric.ruga@gmail.com>
2021-08-29 16:59:33 +0200

    fragment card: improve style

--- src/components/FragmentCard.tsx
+++ src/components/FragmentCard.tsx
@@ -6,14 +6,14 @@ function FragmentCard({ frontmatter: { title, tags }, body }: FragmentM) {
   return (
     <div className="card fragment-card text-dark">
       <div className="card-header">
-        <div className="d-flex justify-content-end">
+        <div className="d-flex justify-content-end pt-2">
           {tags.map((tag) => (
             <div className="badge bg-primary rounded-pill ms-2">{tag}</div>
           ))}
         </div>
-        <h2 className="marker my-4">{title}</h2>
+        <h3 className="marker mx-2 my-4">{title}</h3>
       </div>
-      <div className="card-body">
+      <div className="card-body p-4">
         <MDXRenderer>{body}</MDXRenderer>
       </div>
     </div>
